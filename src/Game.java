d7ee94afb983e6080492ca23c21b232522a908f3
zerogs <zero_gs@list.ru>
2016-11-09 22:25:19 +0300

    Merge branch 'master' of C:\Users\Admin\IdeaProjects\DuckHuntRemake with conflicts.

--- src/Game.java
+++ src/Game.java
@@ -2,40 +2,43 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Game implements Runnable {
+
     /* fields */
+    public GUI gui;
     private int points;
     public List<Duck> ducks = new LinkedList<>();
     private boolean state = true; // true means now playing, false means game over
 
-    private Game(){
+    Game(){
         points = 0;
+        gui = new GUI(this.getInstance());
         Thread th = new Thread(this);
         th.start();
     }
 
     /* Singleton */
-    private static Game instance;
+
+    private static class GameHolder {
+        private final static Game instance = new Game();
+    }
 
     public static Game getInstance(){
-        if (instance == null) {
-            instance = new Game();
-        }
-        return instance;
+        return GameHolder.instance;
     }
 
     @Override
-        public void run(){
-            while(state){
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                GUI.getInstance().repaint();
-                if (Math.random() > 0.95) {
-                    ducks.add(new Duck(Game.getInstance(), GUI.getInstance()));
-                }
+    public void run(){
+        while(state){
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            gui.repaint();
+            if (Math.random() > 0.95) {
+                ducks.add(new Duck(Game.getInstance(), GUI.getInstance()));
+            }
+        }
     }
     public void start(){
 
